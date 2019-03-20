4b5092e5a1c1ac58798c061cdb1b793f9eb779bd
Guiweeneth <ana.arrme@gmail.com>
2019-03-21 00:18:33 +0100

    Commit con menu v1.1

---
+++ pomodoro_menu/ViewModels/ConfigurationPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace pomodoro_menu.ViewModels
+{
+    public class ConfigurationPageViewModel : ContentView
+    {
+        public ConfigurationPageViewModel()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+        }
+    }
+}
+
