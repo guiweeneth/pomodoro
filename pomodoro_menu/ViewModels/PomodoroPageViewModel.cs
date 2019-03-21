9c635918acc233270c0cc4f96af22fe505b8aa3d
Guiweeneth <ana.arrme@gmail.com>
2019-03-21 23:56:24 +0100

    Added timer class and action for configuration

---
+++ pomodoro_menu/ViewModels/PomodoroPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace pomodoro_menu.ViewModels
+{
+    public class PomodoroViewModel : ContentView
+    {
+        public PomodoroViewModel()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+        }
+    }
+}
+
